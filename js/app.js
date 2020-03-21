0bd9b3faf1fd8bd31f3690849fcd2a965f0145fb
idtrone <xlennoah@gmail.com>
2020-03-21 18:59:22 -0500

    implementacion de los eventos de las teclas de la calculadora

--- js/app.js
+++ js/app.js
@@ -1,3 +1,89 @@
 var calculadora={
-    
+    pantalla: "",
+    inicializar: function () {
+        // metodo de escucha de los botones
+        var tecla = document.getElementsByClassName('tecla')
+        for (i in tecla){
+            tecla[i].onmousedown = function (e) {
+                e.currentTarget.style = 'padding: 1px'
+            }
+            tecla[i].onmouseup = function (e) {
+                e.currentTarget.style = 'padding: 0'
+            }
+            tecla[i].onmouseup = function (e) {
+                this.procesarTecla(e.currentTarget.id)
+            }
+        }
+    },
+    procesarTecla(id) {
+        // procesar la tecla segun el valor que tenga
+        switch (id) {
+            case 'on':
+                this.procesarOn()
+                break
+            case 'sign':
+                this.procesarSigno()
+                break
+            case 'raiz':
+                this.procesarRaiz()
+                break
+            case 'menos':
+                this.procesarMenos()
+                break
+            case 'por':
+                this.procesarPor()
+                break
+            case 'suma':
+                this.procesarSuma()
+                break
+            case 'dividido':
+                this.procesarDividido()
+                break
+            case 'punto':
+                this.procesarPunto()
+                break
+            case 'igual':
+                this.procesarIgual()
+                break
+            default:
+                this.procesarNumero(id)
+                break
+        }
+        document.getElementById('display').value = pantalla
+    },
+    procesarOn() {
+
+    },
+    procesarSigno() {
+
+    },
+    procesarRaiz() {
+
+    },
+    procesarMenos() {
+
+    },
+    procesarPor() {
+
+    },
+    procesarSuma() {
+
+    },
+    procesarDividido() {
+
+    },
+    procesarPunto() {
+
+    },
+    procesarIgual() {
+
+    },
+    procesarNumero(id) {
+        if (this.pantalla == 0){
+            this.pantalla = id;
+        }
+    }
 }
+
+
+calculadora.inicializar();
